7e978e3fca393721ecc0aef23e74fa0bc127d1c4
MarineLohezic <lohezic.marine@gmail.com>
2018-12-31 09:16:52 +0100

    modify reservation mock

--- acs/src/main/webapp/app/reservation/reservation.service.ts
+++ acs/src/main/webapp/app/reservation/reservation.service.ts
@@ -10,8 +10,8 @@ export class ReservationService {
     
     get(): Promise<Event[]>{
         return Promise.resolve([
-            {id: 1, start_date: "2018-10-12 09:00", end_date: "2018-10-12 13:00", text: "Event 1"},
-            {id: 2, start_date: "2018-10-13 00:00", end_date: "2018-10-14 13:00", text: "Event 2"},
+            {id: 1, start_date: "2018-12-12 09:00", end_date: "2018-12-12 13:00", text: "Event 1"},
+            {id: 2, start_date: "2018-12-13 00:00", end_date: "2018-12-14 13:00", text: "Event 2"},
         ]);
     }
 }
