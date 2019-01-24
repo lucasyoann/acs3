4563b839f2622cd630eb322af5e761d38c1bbc31
lucasyoann <lucas.yoann29@gmail.com>
2019-01-24 21:47:16 +0100

    ajout reservation au scheduler

--- acs/src/main/webapp/app/reservation/reservation.service.ts
+++ acs/src/main/webapp/app/reservation/reservation.service.ts
@@ -4,10 +4,11 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 
 import { Event } from '../shared/reservation/event';
 import { Reservation } from "src/main/webapp/app/shared/reservation/reservation.entity";
+import { DatePipe } from '@angular/common'
 
 @Injectable()
 export class ReservationService {
-    constructor(private httpClient: HttpClient) {}
+    constructor(private httpClient: HttpClient, public datepipe: DatePipe) {}
        
     getReservation(dateDebut, dateFin): Observable<Reservation[]> {
         let params = new HttpParams();
@@ -17,12 +18,21 @@ export class ReservationService {
     }
     
     transformedReservationToSchedulerEvent(reservations): Promise<Event[]>{
-        let eventList: Event[];
+        let eventList :Event[]= [];
+
         
         reservations.forEach(function(resa) {
-            eventList.push({id: resa.id, start_date: resa.dateEmprunt, end_date: resa.dateRestitution, text: ""});
-        });
-        
+            let evt = new Event();
+            evt.id = resa.id;
+            evt.start_date = this.datepipe.transform( resa.dateEmprunt, 'MM-dd-yyyy HH:mm').toString();
+            evt.end_date = this.datepipe.transform( resa.dateRestitution, 'MM-dd-yyyy HH:mm').toString();
+            evt.text= resa.nom;
+            if(resa.prenom != null){
+                evt.text+=" "+resa.prenom;
+            }
+           
+            eventList.push(evt);
+        }.bind(this));        
         return Promise.resolve(eventList);
     }
 }
